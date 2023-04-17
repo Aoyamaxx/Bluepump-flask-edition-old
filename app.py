9536e3ec24cc9914ba0e988c2fac30eaa23fd3ec
Aoyamaxx <tomako.maketo@gmail.com>
2023-04-17 23:13:40 +0200

    User tracking trial

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-'''
-Author: Kaleem Ullah (k.ullah@uva.nl)
-This program serves as a template for a Flask app to track two variables: 
-(1) time spent on a specified page & (2) whether a specified button clicked or not. 
-Consult ReadMe.pdf for more information.
-'''
-
 from flask import Flask, request, session, render_template, redirect
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
@@ -12,7 +5,7 @@ from flask_session import Session
 
 # Configure app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user_tracking.db'
 
 # Configure flask session
 app.config["SESSION_PERMANENT"] = False
@@ -22,7 +15,6 @@ Session(app)
 # Set up database and database models
 db = SQLAlchemy(app)
 
-# Database model for the continuous variable: time spent
 class PageView(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     visitor_id = db.Column(db.String(10))
@@ -30,111 +22,108 @@ class PageView(db.Model):
     time_spent = db.Column(db.Integer)
     start_time = db.Column(db.DateTime)
 
-# Database model for the binary variable: button click
 class Button(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     visitor_id = db.Column(db.String(10))
-    button = db.Column(db.Boolean)
+    button_name = db.Column(db.String(50))
+    click_count = db.Column(db.Integer)
+    first_click_time = db.Column(db.DateTime)
 
-# Create all the tables for the databases
+class SiteVisit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    visitor_id = db.Column(db.String(10))
+    ip_address = db.Column(db.String(50))
+    user_agent = db.Column(db.String(255))
+    referrer_url = db.Column(db.String(255))
+    entry_time = db.Column(db.DateTime)
+    exit_time = db.Column(db.DateTime)
+    last_page = db.Column(db.String(255))
+
 with app.app_context():
     db.create_all()
 
 # Function to log data: this function saves the time spent on the previous page in the database. Unit of time is seconds. 
 def log_data():
     try:
-        time_spent = (datetime.now() - start_time).total_seconds()
+        time_spent = (datetime.now() - session['start_time']).total_seconds()
 
         # First 3 seconds is the threshold to save the time spent in the database. It is to eliminate recording repetitive page requests/reloads. 
         if time_spent > 3:
             page_view = PageView(
                 visitor_id=session.get('visitor_id'),
-                page=previous_path,
+                page=session['previous_path'],
                 time_spent=time_spent,
-                start_time=start_time)
+                start_time=session['start_time'])
             db.session.add(page_view)
             db.session.commit()
     except:
         pass
 
-##################################################################################
-#
-# After Each Request...
-#
-##################################################################################
+def log_exit():
+    site_visit = SiteVisit.query.filter_by(visitor_id=session.get('visitor_id')).first()
+    if site_visit:
+        site_visit.exit_time = datetime.now()
+        site_visit.last_page = session['previous_path']
+        db.session.commit()
+        
+def get_user_ip():
+    if 'X-Forwarded-For' in request.headers:
+        return request.headers.getlist("X-Forwarded-For")[0].split(',')[0]
+    return request.remote_addr
 
-# after_request decorator of Flask defines actions to be performed after each request coming from the client-side. 
 @app.after_request
 def track_time(response):
-    global start_time
-    global previous_path
-
-    # Every time the user requests default route (/), time spent in the previous path is recorded in the database with log_data(). 
-    if request.path == '/':
-        log_data()
-        # Update start_time and previous_path
-        start_time = datetime.now()
-        previous_path = 'HomePage'
-
-    # Every time the user requests /learn_more route, time spent in the previous path is recorded in the database with log_data(). 
-    if request.path == '/learn_more':
-        log_data()
-        # Update start_time and previous_path
-        start_time = datetime.now()
-        previous_path = 'Learn More'
-
-    # Every time the user requests  /confirmation route, time spent in the previous path is recorded in the database with log_data(). 
-    if request.path == '/confirmation':
-        log_data()
-        try:
-            # Delete start_time and previous_path variables. Time spent on /confirmation route is not recorded. 
-            del start_time, previous_path
-        except:
-            pass
+    if 'start_time' not in session:
+        session['start_time'] = datetime.now()
+    log_data()
+    session['start_time'] = datetime.now()
+    session['previous_path'] = request.path
     return response
-
-##################################################################################
-#
-# Routes
-#
-##################################################################################
 
 @app.route('/')
 def index():
-    # Getting the unique id from the home page URL. The unique URL will be generated by Qualtrics for each visitor. 
     visitor_id = request.args.get('uid')
-    # Add visitor_id to the session
     if visitor_id:
         session["visitor_id"] = visitor_id
+        site_visit = SiteVisit(
+            visitor_id=visitor_id,
+            entry_time=datetime.now(),
+            ip_address=get_user_ip(),
+            user_agent=request.user_agent.string,
+            referrer_url=request.referrer)
+        db.session.add(site_visit)
+        db.session.commit()
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-@app.route('/learn_more')
-def learn_more():
-    return render_template('learn_more.html')
+@app.route('/map')
+def map():
+    return render_template('map.html')
 
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
 
-@app.route('/confirmation')
-def confirmation():
-    return render_template('done.html')
+@app.route('/donate')
+def donate():
+    return render_template('donate.html')
 
+@app.route('/track_button_click/<string:button_name>')
+def track_button_click(button_name):
+    visitor_id = session.get('visitor_id')
+    button = Button.query.filter_by(visitor_id=visitor_id, button_name=button_name).first()
 
-# /log_binary is the route that users are sent to when they click on the "Contact" button. 
-# However, it is a dummy route which does not render a new template. It redirects users to the Home Page. 
-# "Contact" button is added to provide an example structure for a button-click data collection. 
-# button_tracking() function saves the visitor_id in the database if the visitor clicked on the "Contact" button.  
-@app.route("/log_binary")
-def button_tracking():
-    try:
-        button_click = Button(
-            visitor_id=session.get('visitor_id'),
-            button=True)
-        db.session.add(button_click)
-        db.session.commit()
-    except:
-        pass
-    return redirect('/')
+    if not button:
+        button = Button(visitor_id=visitor_id, button_name=button_name, click_count=1, first_click_time=datetime.now())
+        db.session.add(button)
+    else:
+        button.click_count += 1
 
+    db.session.commit()
+    return '', 204
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+    app.run()
