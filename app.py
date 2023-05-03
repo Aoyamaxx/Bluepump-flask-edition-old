8ac4840fde4acb2e6cb0e1ce5c8fb1340027ccc2
Aoyamaxx <tomako.maketo@gmail.com>
2023-05-04 00:12:38 +0200

    First worked user tracking update

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, request, session, render_template, redirect, send_from_
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
+from flask import jsonify
+import uuid
+import json
+import urllib.parse
 
 # Configure app
 app = Flask(__name__)
@@ -15,23 +19,9 @@ Session(app)
 # Set up database and database models
 db = SQLAlchemy(app)
 
-class PageView(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    visitor_id = db.Column(db.String(10))
-    page = db.Column(db.String(255))
-    time_spent = db.Column(db.Integer)
-    start_time = db.Column(db.DateTime)
-
-class Button(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    visitor_id = db.Column(db.String(10))
-    button_name = db.Column(db.String(50))
-    click_count = db.Column(db.Integer)
-    first_click_time = db.Column(db.DateTime)
-
 class SiteVisit(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    visitor_id = db.Column(db.String(10))
+    visitor_id = db.Column(db.String(36))
     ip_address = db.Column(db.String(50))
     user_agent = db.Column(db.String(255))
     referrer_url = db.Column(db.String(255))
@@ -39,81 +29,161 @@ class SiteVisit(db.Model):
     exit_time = db.Column(db.DateTime)
     last_page = db.Column(db.String(255))
 
+class DonatePopup(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    visitor_id = db.Column(db.String(36))
+    action = db.Column(db.String(50))
+
+    def __init__(self, visitor_id, action):
+        self.visitor_id = visitor_id
+        self.action = action
+
+class PrivacyPolicy(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    visitor_id = db.Column(db.String(36), nullable=False)
+    decision = db.Column(db.String(2), nullable=False)  # 'Y' for accept, 'N' for reject, 'NA' for no action recorded
+
+    def __init__(self, visitor_id, decision):
+        self.visitor_id = visitor_id
+        self.decision = decision
+
 with app.app_context():
     db.create_all()
 
-# Function to log data: this function saves the time spent on the previous page in the database. Unit of time is seconds. 
-def log_data():
-    try:
-        time_spent = (datetime.now() - session['start_time']).total_seconds()
+# Function to log data: this function saves the time spent on the previous page in the database. Unit of time is seconds.
+def generate_visitor_id():
+    return str(uuid.uuid4())[:10]
+    
+def decode_url(url):
+    return urllib.parse.unquote(url)
 
-        # First 3 seconds is the threshold to save the time spent in the database. It is to eliminate recording repetitive page requests/reloads. 
-        if time_spent > 3:
-            page_view = PageView(
-                visitor_id=session.get('visitor_id'),
-                page=session['previous_path'],
-                time_spent=time_spent,
-                start_time=session['start_time'])
-            db.session.add(page_view)
-            db.session.commit()
-    except:
-        pass
+def log_site_visit(visitor_id):
+    referrer = request.headers.get('Referer', None)
+    site_visit = SiteVisit(
+        visitor_id=visitor_id,
+        entry_time=datetime.now(),
+        ip_address=get_user_ip(),
+        user_agent=request.user_agent.string,
+        referrer_url=referrer)
+    db.session.add(site_visit)
+    db.session.commit()
 
-def log_exit():
-    site_visit = SiteVisit.query.filter_by(visitor_id=session.get('visitor_id')).first()
-    if site_visit:
-        site_visit.exit_time = datetime.now()
-        site_visit.last_page = session['previous_path']
-        db.session.commit()
-        
-def get_user_ip():
-    if 'X-Forwarded-For' in request.headers:
-        return request.headers.getlist("X-Forwarded-For")[0].split(',')[0]
-    return request.remote_addr
-
-@app.after_request
-def track_time(response):
-    if 'start_time' not in session:
-        session['start_time'] = datetime.now()
-    log_data()
-    session['start_time'] = datetime.now()
-    session['previous_path'] = request.path
-    return response
-
-@app.route('/')
-def index():
-    visitor_id = request.args.get('uid')
-    if visitor_id:
-        session["visitor_id"] = visitor_id
+def log_site_visit_once(visitor_id):
+    if 'site_visit_logged' not in session or not session['site_visit_logged']:
+        referrer = request.headers.get('Referer', None)
         site_visit = SiteVisit(
             visitor_id=visitor_id,
             entry_time=datetime.now(),
             ip_address=get_user_ip(),
             user_agent=request.user_agent.string,
-            referrer_url=request.referrer)
+            referrer_url=referrer)
         db.session.add(site_visit)
         db.session.commit()
-    return render_template('index.html')
+        session['site_visit_logged'] = True
+
+def get_user_ip():
+    if 'X-Forwarded-For' in request.headers:
+        return request.headers.getlist("X-Forwarded-For")[0].split(',')[0]
+    return request.remote_addr
+
+def save_user_action(visitor_id, action):
+    donate_popup = DonatePopup(visitor_id=visitor_id, action=action)
+    db.session.add(donate_popup)
+    db.session.commit()
+    
+def log_exit(last_page):
+    site_visit = SiteVisit.query.filter_by(visitor_id=session.get('visitor_id')).order_by(SiteVisit.id.desc()).first()
+    if site_visit:
+        site_visit.exit_time = datetime.now()
+        site_visit.last_page = last_page
+        db.session.commit()
+    
+@app.route('/track_exit', methods=['POST'])
+def track_exit_route():
+    last_page = request.form.get('last_page')
+    log_exit(last_page)
+    return '', 204
+
+@app.route('/get_visitor_id', methods=['GET'])
+def get_visitor_id():
+    visitor_id = str(uuid.uuid4())
+    return jsonify(visitor_id=visitor_id)
+
+@app.route('/privacy_banner')
+def privacy_banner():
+    return render_template('privacy_banner.html')
+
+@app.route('/learn_more')
+def learn_more():
+    visitor_id = session.get("visitor_id", generate_visitor_id())
+    log_site_visit_once(visitor_id)
+    return render_template('learn_more.html')
+
+@app.route('/log_privacy_decision', methods=['POST'])
+def log_privacy_decision():
+    visitor_id = request.form['visitor_id']
+    decision = request.form['decision']
+    privacy_policy_record = PrivacyPolicy(visitor_id, decision)
+    db.session.add(privacy_policy_record)
+    db.session.commit()
+    return jsonify(success=True)
+
+@app.route('/track_user_action', methods=['POST'])
+def track_user_action():
+    visitor_id = request.form.get('visitor_id')
+    if not visitor_id:
+        visitor_id = session.get('visitor_id')
+    action = request.form.get('action')
+
+    # Save the action to the database (you need to implement this function)
+    save_user_action(visitor_id, action)
+
+    return 'OK'
+
+@app.route('/donate_popup')
+def donate_popup():
+    return render_template('donate_popup.html')
+
+@app.route('/')
+def index():
+    visitor_id = request.args.get('uid')
+    if not visitor_id:
+        visitor_id = generate_visitor_id()
+    session["visitor_id"] = visitor_id
+    log_site_visit(visitor_id)
+    log_site_visit_once(visitor_id)
+    return render_template('index.html', visitor_id=visitor_id)
 
 @app.route('/about')
 def about():
+    visitor_id = session.get("visitor_id", generate_visitor_id())
+    log_site_visit_once(visitor_id)
     return render_template('about.html')
 
 @app.route('/map')
 def map():
+    visitor_id = session.get("visitor_id", generate_visitor_id())
+    log_site_visit_once(visitor_id)
     return render_template('map.html')
 
 @app.route('/projects')
 def projects():
+    visitor_id = session.get("visitor_id", generate_visitor_id())
+    log_site_visit_once(visitor_id)
     return render_template('projects.html')
 
 @app.route('/donate')
 def donate():
+    visitor_id = session.get("visitor_id", generate_visitor_id())
+    log_site_visit_once(visitor_id)
     return render_template('donate.html')
 
 @app.route('/siaya')
 def siaya():
+    visitor_id = session.get("visitor_id", generate_visitor_id())
+    log_site_visit_once(visitor_id)
     return render_template('siaya.html')
+
 
 @app.route('/header')
 def header():
@@ -122,20 +192,6 @@ def header():
 @app.route('/footer')
 def footer():
     return render_template('footer.html')
-
-@app.route('/track_button_click/<string:button_name>')
-def track_button_click(button_name):
-    visitor_id = session.get('visitor_id')
-    button = Button.query.filter_by(visitor_id=visitor_id, button_name=button_name).first()
-
-    if not button:
-        button = Button(visitor_id=visitor_id, button_name=button_name, click_count=1, first_click_time=datetime.now())
-        db.session.add(button)
-    else:
-        button.click_count += 1
-
-    db.session.commit()
-    return '', 204
 
 if __name__ == '__main__':
     app.run()
--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,199 @@
-$(function(){
-  $("#header").load("header.html");
-  $("#footer").load("footer.html");
-  
-  // Add event listener to Donate buttons
-  $('body').on('click', '.donate', function() {
-    $.get('/track_button_click/donate_button');
+(function () {
+
+  function logSiteVisit(visitorId) {
+    $.ajax({
+      url: '/log_site_visit',
+      method: 'POST',
+      data: {
+        visitor_id: visitorId,
+      },
+    });
+  }
+
+  function logSiteExit(visitor_id) {
+  $.ajax({
+    url: '/track_exit',
+    method: 'POST',
+    data: {
+      visitor_id: visitor_id,
+      last_page: location.pathname
+    }
+  });
+}
+
+  function fetchVisitorId(callback) {
+    $.ajax({
+      url: '/get_visitor_id',
+      method: 'GET',
+      success: function (response) {
+        callback(response.visitor_id);
+      },
+    });
+  }
+
+  function initializeEvents(visitorId) {
+    let startTimeGlobal = new Date();
+
+    // Log the site visit and set the visitor_id in sessionStorage
+    logSiteVisit(visitorId);
+    sessionStorage.setItem('visitor_id', visitorId);
+
+    window.addEventListener('beforeunload', () => {
+      logSiteExit(visitorId);
+    });
+
+    // Event listeners and other code that depends on visitorId
+    $('.close, .not-now').on('click', function () {
+      hideDonatePopup();
+      trackUserAction(visitorId, 'closed_popup_no');
+    });
+
+    $('body').on('click', '#donate-button', function () {
+      hideDonatePopup();
+      trackUserAction(visitorId, 'clicked_donate');
+      // Redirect the user to the donate page
+      window.location.href = '/donate';
+    });
+
+    $('body').on('click', '#donate-popup .not-now', function () {
+      trackUserAction(visitorId, 'closed_popup_not_now');
+    });
+
+    $('body').on('click', '#donate-popup .close', function () {
+      trackUserAction(visitorId, 'closed_popup_close');
+    });
+
+    $('body').on('click', '#learn-more-link', function () {
+      trackUserAction(visitorId, 'clicked_learn_more');
+    });
+
+    // Add event listener to Donate buttons
+    $('body').on('click', '.donate', function () {
+      $.get(`/track_button_click/donate_button?visitor_id=${visitorId}`);
+    });
+
+    // Add event listener to Map button
+    $('body').on('click', '.menu-item[href="map.html"]', function () {
+      $.get(`/track_button_click/map_button?visitor_id=${visitorId}`);
+    });
+
+    $('#accept-privacy, #reject-privacy').on('click', function () {
+      const decision = this.id === 'accept-privacy' ? 'Y' : 'N';
+      sessionStorage.setItem('privacy_decision', decision);
+      $('#privacy-banner').hide();
+
+    if (typeof serverVisitorId !== 'undefined' && serverVisitorId !== '') {
+      initializeEvents(serverVisitorId);
+    } else {
+    fetchVisitorId(function (visitorId) {
+      initializeEvents(visitorId);
+    });
+  }
+
+      // Record user decision in your database
+      $.ajax({
+        url: '/log_privacy_decision',
+        method: 'POST',
+        data: {
+          visitor_id: serverVisitorId,
+          decision: decision,
+        },
+      });
+    });
+  }
+
+  if (typeof serverVisitorId !== 'undefined' && serverVisitorId !== '') {
+    initializeEvents(serverVisitorId);
+  } else {
+    fetchVisitorId(function (visitorId) {
+      initializeEvents(visitorId);
+    });
+  }
+
+  function trackUserAction(visitorId, action) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/track_user_action', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(`visitor_id=${visitorId}&action=${action}`);
+  }
+
+  function showDonatePopup() {
+  console.log('Showing popup');
+  $('#donate-popup').fadeIn(300);
+  }
+
+  function hideDonatePopup() {
+    $('#donate-popup').fadeOut(300);
+  }
+
+  let timeElapsed = 0;
+  const timeToShowPopup = 10000;
+
+  function checkUserAction() {
+    timeElapsed += 1000;
+    console.log("Time elapsed:", timeElapsed);
+
+    // Check if the popup has already been shown
+    const popupShown = sessionStorage.getItem('popup_shown');
+
+    if (timeElapsed >= timeToShowPopup && !popupShown) {
+      showDonatePopup();
+
+      // Set a flag in sessionStorage to indicate the popup has been shown
+      sessionStorage.setItem('popup_shown', true);
+    }
+  }
+
+  $(document).ready(function () {
+    $("#header").load("header.html");
+    $("#footer").load("footer.html");
+
+    $("#load-privacy-banner").load("/privacy_banner", function () {
+    // After the content is loaded, append it to the body
+    $('body').append($('#load-privacy-banner').html());
+    $('#load-privacy-banner').remove();
+
+    // Only show the privacy banner if the user hasn't made a decision yet
+    if (!sessionStorage.getItem('privacy_decision')) {
+      $('#privacy-banner').show();
+    }
+
+    // Handle privacy banner actions
+    $('#accept-privacy, #reject-privacy').on('click', function () {
+      const decision = this.id === 'accept-privacy' ? 'Y' : 'N';
+      sessionStorage.setItem('privacy_decision', decision);
+      $('#privacy-banner').hide();
+
+      // Record user decision in your database
+      $.ajax({
+        url: '/log_privacy_decision',
+        method: 'POST',
+        data: {
+          visitor_id: serverVisitorId,
+          decision: decision
+        }
+      });
+    });
+
+    $('#privacy-policy-link').on('click', function () {
+      sessionStorage.setItem('privacy_decision', 'P');
+      $('#privacy-banner').hide();
+    });
   });
 
-  // Add event listener to Map button
-  $('body').on('click', '.menu-item[href="map.html"]', function() {
-    $.get('/track_button_click/map_button');
+    // Load the donate_popup content
+    $('#load-donate-popup').load('/donate_popup', function () {
+      // After the content is loaded, append it to the body
+      $('body').append($('#load-donate-popup').html());
+      $('#load-donate-popup').remove();
+
+      // Add the event listeners for closing the popup and clicking the donate button
+      $('.close, .not-now').on('click', function () {
+        hideDonatePopup();
+      });
+    });
+
+    // Check if the user is on the donate page or clicked the donate button within 15 seconds
+    const checkUserActionInterval = setInterval(checkUserAction, 1000);
   });
-});
+})();
